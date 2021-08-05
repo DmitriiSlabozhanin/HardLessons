969b08b40453f55471c265c015ae563f07c5860e
DmitriiSlabozhanin <senya@vtomske.ru>
2021-08-05 19:05:33 +0700

    add lesson05hard

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
 'use strict';
-
-function countSymbol(str)
-{
-  if (typeof str !== 'string') {
-    alert('Переданное значение не является строкой!');
-    return;
-  }
-  str = str.trim(); 
-  return str.length > 30 ? str.slice(0, 30) + '...' : str;
-}
-
-//let result = countSymbol(1111);
-let result = countSymbol(" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labor ");
-console.log(result);
