4cd1eec1b82547d7e2e7f1e880060028fb1fe1ea
DmitriiSlabozhanin <senya@vtomske.ru>
2021-08-15 23:13:30 +0700

    add lesson 07hard

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
 'use strict';
 
-let arr = ['4463689', '834763', '2976842', '186352365', '4863486', '7450222', '245693068'];
-
-arr.forEach((num) => {
-    if (num.startsWith('2') || num.startsWith('4')) {
-        console.log(num);
-  }
-});
-
-
-function numb(n) {
-    for (let i = 2; i * i <= n; i === 2 ? i++ : i += 2) {
-        if (n % i === 0) {
-            return false;
-        }
-    }
-    return n > 1;
-  }
-
-const res = [...Array(101)].reduce((a, _, i) => a.concat(numb(i) ? console.log(`Делители числа ${i}: 1 и ${i}`) : []) , []).join('\n');
+let days = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "<i>Суббота</i>", "<i>Воскресенье</i>"];
+    
+let toDay = (6 + new Date().getDay()) % 7;
+days[toDay] = `<b>${ days[toDay] }</b>`;
+  
+document.body.insertAdjacentHTML("beforeEnd", days.join("<br>"));
